c67bb1be368a59c588ecf6cd12aa26237a97c7b9
METALbrasier2X0 <ra-phi64@hotmail.fr>
2023-05-12 21:43:00 +0200

    added time and volume

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect, useRef } from "react";
 
+import { FiVolume2 } from "react-icons/fi";
+import { FiVolume1 } from "react-icons/fi";
+import { FiVolumeX } from "react-icons/fi";
 
 /**
  * Code to display the menu
@@ -8,8 +11,13 @@ import { useState, useEffect, useRef } from "react";
  */
 
 function Menu(props) {
-
   
+  var today = new Date();
+
+  const [Time, setTime] = useState(today.getHours() + ':' + today.getMinutes());
+  const [Volume, setVolume] = useState(null);
+
+  setInterval(() => setTime(today.getHours() + ':' + today.getMinutes() ), 1000);
 
   return (
 
@@ -17,8 +25,11 @@ function Menu(props) {
     <div className='banner'> 
     <div className='info'>this is placeholder info / I will say whatever i need to say here / i aint dead</div>
         <div className='logo'> <img></img></div>
+        <div className='topleft'>
+        <div className='hour'>| {Time} |</div>
+        <div className='volume'> <FiVolume2 /> </div>
+        </div>
        
-        <div className='volume'></div>
     </div>
    </>
 
